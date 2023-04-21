be92d6202844152a862066f51e191962de69ede7
Alejandro Cortes Mora <126348226+AlejoCM03@users.noreply.github.com>
2023-04-21 17:05:55 -0600

    Add files via upload
    comando "ayuda" para proporcionar el listado de comandos

---
+++ ayuda.sh
@@ -0,0 +1,53 @@
+
+
+
+#!/bin/bash
+echo ""
+echo ""
+echo ""
+
+echo "     |7|7|7|      \7\     /7/   |7|7|    |7|7|  |7|7|7|7|7\           |7|7|7|       " 
+echo "    /7/    \7\     \7\   /7/    |7|7|    |7|7|  |7|7|     \7\        /7/    \7\     "
+echo "   /7/  __  \7\     \7\ /7/     |7|7|    |7|7|  |7|7|     \7\7\     /7/  __  \7\   "
+echo "  /7/  /  \  \7\     |7|7|      |7|7|    |7|7|  |7|7|     \7\7\    /7/  /  \  \7\  "
+echo " /7/  /____\  \7\    |7|7|      \7\7\    /7/7/  |7|7|     \7\     /7/  /____\  \7\  "
+echo "/7/            \7\   |7|7|        |7|7|7|7|7|   |7|7|7|7|7\      /7/            \7\ "
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[1;35m             -> ayuda       \e[0m    \e[36;4mProporciona un listado de comandos, con una brebe descripcion, que el usuario puede ejecutar \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> infosis     \e[0m    \e[36;4mMuestra la informacion del sistema operativo: \e[1mMemoria RAM, Version del SO, Arquitectura del Sistema \e[0m \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> fecha       \e[0m    \e[36;4mProporciona la fecha y hora con el formato: \e[1m charDia CharMes Dia Hora:Minuto:Segundo Año Dia/Mes/Año \e[0m \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> buscar      \e[0m    \e[36;4mBusca un archivo dentro de una carpeta, utilizando como paramteros:\e[1m carpeta_a_buscar archivo_a_buscar \e[0m \e[0m \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> creditos    \e[0m    \e[36;4mMuestra los creditos de los autores de esta bonita terminal \e[0m \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> gameGato    \e[0m    \e[36;4mEjecuta el juego del gato \e[0m \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> reproductor \e[0m    \e[36;4mEjecuta el reproductor MP3 \e[0m \e[0m"
+
+echo ""
+echo ""
+echo -e "\e[1;35m             -> salir       \e[0m    \e[36;4mPermite salir de la terminal \e[0m \e[0m"
+
+echo ""
+echo ""
+echo ""
+
